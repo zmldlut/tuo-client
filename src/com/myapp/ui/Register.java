ea422c9a05fdbf0b645836e1d6a1fa14b9b4b18f
zml <zml@zml-PC>
2013-12-12 10:26:07 +0800

    合并最新的拖后腿客户端代码

--- src/com/myapp/ui/Register.java
+++ src/com/myapp/ui/Register.java
@@ -1,23 +1,23 @@
 package com.myapp.ui;
 
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Calendar;
 import java.util.List;
 import java.util.Map;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.app.AlertDialog.Builder;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.myapp.R;
@@ -28,19 +28,13 @@ public class Register extends Activity {
 	private EditText r_password1;
 	private EditText r_password2;
 	private ImageButton r_cancel;
-	private Spinner r_year;
-	private Spinner r_month;
-	private Spinner r_day;
+	private Button r_date;
 	private ImageButton r_verify;
+	
+	ButtonListener buttonListener;
 	
 	private int[] image = {R.drawable.pic1,R.drawable.pic2,R.drawable.pic3,R.drawable.pic4,R.drawable.pic5,
 			R.drawable.pic6,R.drawable.pic7,R.drawable.pic8,R.drawable.pic9,R.drawable.pic10};
-	private final String[] year = {"1999","2000","2001"};
-	private final String[] month = {"1","2","3"};
-	private final String[] day ={"1","2","3"};
-	private ArrayAdapter<String> adapter_year;
-	private ArrayAdapter<String> adapter_month;
-	private ArrayAdapter<String> adapter_day;
 	
 	private String r_user_account;
 	private String r_user_password1;
@@ -53,66 +47,64 @@ public class Register extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.register);
 		
-//		r_submit = (Button)findViewById(R.id.b_submit);
-//		r_account = (EditText)findViewById(R.id.et_account);
-//		r_password1 = (EditText)findViewById(R.id.et_password1);
-//		r_password2 = (EditText)findViewById(R.id.et_password2);
-//		r_cancel = (ImageButton)findViewById(R.id.img_b_cancel);
-//		r_year = (Spinner)findViewById(R.id.s_year);
-//		r_month = (Spinner)findViewById(R.id.s_month);
-//		r_day = (Spinner)findViewById(R.id.s_day);
-//		r_verify = (ImageButton)findViewById(R.id.ib_verify);
-//		
-//		ButtonListener buttonListener = new ButtonListener();
-//		
-//
-//		
-//		r_submit.setOnClickListener(buttonListener);
-//		r_cancel.setOnClickListener(buttonListener);
-//		r_verify.setOnClickListener(buttonListener);
-//
-//		
-//		
-//		spinnerEvent();
-		
-//		setEvent();
-
+		getWidget();
+		setEvent();
 	}
 	
-//	public void setEvent() {
-//		for(int i=1;i<=12;i++) {
-//			year[i-1]=;
-//		}
-//	}
-	
-	public void spinnerEvent() {
-		adapter_year = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,
-				year);
-		adapter_month = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,
-				month);
-		adapter_day = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,
-				day);
-		
-		r_year.setAdapter(adapter_year);
-		r_month.setAdapter(adapter_month);
-		r_day.setAdapter(adapter_day);
-		
-		r_year.setVisibility(View.VISIBLE);
-		r_month.setVisibility(View.VISIBLE);
-		r_day.setVisibility(View.VISIBLE);
-	}
 
 	
+	public void getWidget(){
+		r_submit = (Button)findViewById(R.id.b_submit);
+		r_account = (EditText)findViewById(R.id.et_account);
+		r_password1 = (EditText)findViewById(R.id.et_password1);
+		r_password2 = (EditText)findViewById(R.id.et_password2);
+		r_cancel = (ImageButton)findViewById(R.id.img_b_cancel);
+		r_date = (Button)findViewById(R.id.b_date);
+		r_verify = (ImageButton)findViewById(R.id.ib_verify);
+		
+		buttonListener = new ButtonListener();
+	}
+	
+	public void setEvent() {
+		r_submit.setOnClickListener(buttonListener);
+		r_cancel.setOnClickListener(buttonListener);
+		r_verify.setOnClickListener(buttonListener);
+		r_date.setOnClickListener(buttonListener);
+	}
+	
+
+	
+	@SuppressLint("NewApi")
 	class ButtonListener implements OnClickListener {
-		@SuppressLint("NewApi")
 		public void onClick(View v) {
 			int id = v.getId();
 			Intent intent = new Intent();
 			switch (id) {
+			case R.id.b_date:
+				Calendar cal = Calendar.getInstance();
+				DatePickerDialog.OnDateSetListener dateListener = 
+						new DatePickerDialog.OnDateSetListener() {
+							
+							@Override
+							public void onDateSet(DatePicker view, int year, int monthOfYear,
+									int dayOfMonth) {
+								// TODO Auto-generated method stub
+								monthOfYear+=1;
+								r_date.setText(year + "-" + monthOfYear + "-" + dayOfMonth);
+							}
+				};
+				
+				Dialog dateDialog = new DatePickerDialog(Register.this,
+						dateListener,
+						cal.get(Calendar.YEAR),
+						cal.get(Calendar.MONTH),
+						cal.get(Calendar.DAY_OF_MONTH));
+				dateDialog.show();
+				break;
+		
 			case R.id.img_b_cancel:
 				r_cancel.setImageResource(R.drawable.arrow_left_pressed);
-				intent.setClass(Register.this,MainActivity.class);
-				startActivity(intent);
+
 				finish();
 				break;
 			case R.id.ib_verify:
@@ -124,22 +116,19 @@ public class Register extends Activity {
 				r_user_password1 = r_password1.getText().toString();
 				r_user_password2 = r_password2.getText().toString();
 				if(r_user_password1.isEmpty() || r_user_password2.isEmpty() || r_user_account.isEmpty() ) {
-					new Builder(Register.this).setMessage("锟斤拷息锟斤拷锟斤拷为锟斤拷").show();
+					Toast.makeText(Register.this, "注册信息不能为空", Toast.LENGTH_SHORT).show();
 				}else {
 					if(r_user_password1.equals(r_user_password2) ){
 						if(r_user_password1.equals(r_user_account)) {
-							new Builder(Register.this).setMessage("锟剿猴拷锟斤拷锟诫不锟斤拷一锟斤拷").show();
+							Toast.makeText(Register.this, "账号密码不能相同", Toast.LENGTH_SHORT).show();
 						}else {
 							
-							addList();
-							
-							Toast.makeText(Register.this, "注锟斤拷晒锟�", Toast.LENGTH_SHORT).show();
-							intent.setClass(Register.this,MainActivity.class);
-							startActivity(intent);
+							Toast.makeText(Register.this, "注册成功", Toast.LENGTH_SHORT).show();
+
 							finish();
 						}
 					}else{
-					new Builder(Register.this).setMessage("锟斤拷锟诫不一锟斤拷").show();
+						Toast.makeText(Register.this, "密码不一致", Toast.LENGTH_SHORT).show();
 					}
 				}
 				break;
@@ -148,13 +137,7 @@ public class Register extends Activity {
 		}
 	}
 	
-	public void addList() {
-		Map<String, Object> map = new HashMap<String, Object>();
 
-		map.put("userAccount", r_user_account);
-		map.put("userPassword", r_user_password1);
-		dataList.add(map);
-	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
